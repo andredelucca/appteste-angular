ca934d01a7a2060c0ceb34c7cea21d1b61248a73
Vendas 02 <andre.delucca80@gmail.com>
2021-03-19 10:58:39 -0300

    aplicativo com angular criado

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'Um grande telefone com uma das melhores telas'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'Um ótimo telefone com uma das melhores câmeras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
