9d511c26ec206ab92c835b66a9cab0aab9b6919e
Vendas 02 <andre.delucca80@gmail.com>
2021-03-19 15:46:42 -0300

    início da aplicação e inclusão de image

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,20 +1,27 @@
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'Um grande telefone com uma das melhores telas'
+    imageUrl:"assets/image/iphone8.jpg",
+    name: 'Iphone8',
+    price: '3.999,00',
+    description: 'O melhor telefone da Apple'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
+    name: 'Telefone Mini',
+    price: '699,00',
     description: 'Um ótimo telefone com uma das melhores câmeras'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
+    name: 'Telefone Standard',
+    price: '299,00',
+    description: ''
+  },
+  {
+    id: 4,
+    name: 'Telefone',
+    price: '500,00',
     description: ''
   }
 ];
